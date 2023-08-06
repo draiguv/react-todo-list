76b1b167cc78844dff82a5c0d4b6b785ac1f8331
draiguv <dcrauv13@gmail.com>
2023-08-06 17:43:36 +0800

    Add some functions in input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
 
 export default function App() {
+  const [item, setItem] = useState('');
+
+  const handleInput = (e) => {
+    console.log(e.target.value);
+    setItem(e.target.value);
+  };
+
   return (
-    <div className="container">
-      <div className="heading">
+    <div className='container'>
+      <div className='heading'>
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input type="text" />
+      <div className='form'>
+        <input
+          type='text'
+          onChange={handleInput}
+          value={item}
+          placeholder='Enter item'
+        />
         <button>
           <span>Add</span>
         </button>
